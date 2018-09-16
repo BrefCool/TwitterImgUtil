09a5b3d4f4a100036ef621286055be702feee076
br3fc00l <461761446@qq.com>
2018-09-15 22:28:19 -0400

    improve video quality

--- TwitterImgUtil/selfFFMPEG.py
+++ TwitterImgUtil/selfFFMPEG.py
@@ -1,10 +1,21 @@
 import subprocess
 import os
+import time
+
+def resize_images(screen_name):
+    image_path = 'download_images/' + screen_name
+    if not os.path.exists(image_path+'/for_video'):
+        os.mkdir(image_path+'/for_video')
+    cmd = ['ffmpeg', '-i', image_path+'/image-%05d.jpg', '-vf', 'scale=640:480',
+           image_path+'/for_video/image-%05d.png']
+    subprocess.call(cmd)
 
 def convert_images_to_video(screen_name):
-    image_path = 'download_images/' + screen_name + '/image-%05d.jpg'
+    resize_images(screen_name)
+    image_path = 'download_images/' + screen_name + '/for_video'
     output_path = 'download_images/' + screen_name + '/' + screen_name + '_movie.mp4'
     if os.path.exists(output_path):
         os.remove(output_path)
-    cmd = ['ffmpeg', '-i', image_path, '-framerate', '1/5', output_path]
+    cmd = ['ffmpeg', '-r', '1/2', '-i', image_path+'/image-%05d.png', '-c:v',
+           'libx264', '-r', '30', '-pix_fmt', 'yuv420p', output_path]
     subprocess.call(cmd)
